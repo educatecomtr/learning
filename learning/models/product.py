3f40f64639b9306a0be9b3130eafa2029580f799
Emre Çevik <info@internet.com.tr>
2019-04-11 02:03:10 +0300

    create git

--- learning/models/product.py
+++ learning/models/product.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 
 
 class Product(models.Model):
-    # id = models.AutoField(primary_key=True)
+    # benzersiz_key = models.AutoField(primary_key=True)
     name = models.CharField(verbose_name='Ürün İsmi', max_length=200)
     content = models.TextField(verbose_name='Ürün Açıklaması')
     created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        # db_table = 'urunler'
+        #db_table = 'urunler'
         verbose_name = 'Ürün'
         verbose_name_plural = 'Ürünler'
 
