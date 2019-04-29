d6bc0db27eb1b9774f7d7eff8e90e5a2911de73a
Emre Cevik <info@internet.com.tr>
2019-04-29 17:50:00 +0300

    push

--- project/admin.py
+++ project/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 from .models import Dealer, Distributor, Product, Order, Brand, Category
 
+
+class ProductAdmin(admin.ModelAdmin):
+    # fields = ('name',)
+    # readonly_fields = ('name', )
+    exclude = ('slug', )
+    list_display = ('name', 'created')
+    list_filter = ('created',)
+    change_list_template = 'admin/learning/products_change_list.html'
+
+
+admin.site.site_header = 'Stok Yönetimi'
+
 # Register your models here.
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Dealer)
 admin.site.register(Distributor)
-admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Brand)
 admin.site.register(Category)
+
+# admin.site.unregister(Group)
