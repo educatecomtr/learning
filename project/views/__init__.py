aede8770b17f1cd5bbc06695bf3548e1973165e2
Emre Çevik <info@internet.com.tr>
2019-05-06 18:46:48 +0300

    create git

--- project/views/__init__.py
+++ project/views/__init__.py
@@ -2,4 +2,4 @@ from .accounts import RoleView, DealerRoleView, DistributorRoleView, list_staff,
 from .accounts import edit_permissions, attach_staff
 from .dealer import DealerCreateView, DealerUpdateView, DealerListView, DealerDeleteView
 from .product import ProductCreateView, ProductUpdateView, ProductListView, ProductDeleteView
-from .shop import ShopListView
+from .shop import ShopListView, ShopDetailView
