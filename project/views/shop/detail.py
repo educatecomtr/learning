79474c5999bb5ec19e4dc3e490855c9e25766b96
Emre Cevik <info@internet.com.tr>
2019-05-06 18:59:44 +0300

    push

---
+++ project/views/shop/detail.py
@@ -0,0 +1,10 @@
+from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from project.models import Product, Dealer
+from stocks.mixins import CheckDealerMixin
+
+
+class ShopDetailView(CheckDealerMixin, LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = "project/shop/detail.html"
+    context_object_name = 'product'
